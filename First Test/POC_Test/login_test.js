8b36ef23870823d27baf03c3a81b39ebd5b2ee42
Mert <m.karaomer1@gmail.com>
2022-07-28 13:31:51 +0300

    Update login_test.js
    fix

--- First Test/POC_Test/login_test.js
+++ First Test/POC_Test/login_test.js
@@ -10,3 +10,35 @@ Scenario('yanlış kullanıcı adı', ({ I }) => {
 
 
 });
+Scenario('doğru kullanıcı adı yanlış şifre @testcase2', ({ I }) => {
+    I.amOnpage("https://amazon.com.tr");
+    I.waitForElement("//div[@id='nav-singin-tooltip']");
+    I.click("//div[@id='nav-singin-tooltip']");
+    I.waitForElement("//input[@id='ap_email']");
+    I.fillField("//input[@id='ap_email']","mert.karaomer@ogr.sakarya.edu.tr");
+    I.waitForElement("//input[@id='continue']");
+    I.click("//input[@id='continue']");
+    I.waitForElement("//input[@id='ap_password']");
+    I.fillField("//input[@id='ap_password']","qweer");
+    I.waitForElement("//input[@id='signInSubmit']");
+    I.click("//input[@id='signInSubmit']");
+    I.seeElement("//*[contains(test(),'ımportant Message')]");
+
+
+});
+Scenario('doğru kullanıcı adı doğru şifre @testcase3', ({ I }) => {
+    I.amOnpage("https://amazon.com.tr");
+    I.waitForElement("//div[@id='nav-singin-tooltip']");
+    I.click("//div[@id='nav-singin-tooltip']");
+    I.waitForElement("//input[@id='ap_email']");
+    I.fillField("//input[@id='ap_email']","mert.karaomer@ogr.sakarya.edu.tr");
+    I.waitForElement("//input[@id='continue']");
+    I.click("//input[@id='continue']");
+    I.waitForElement("//input[@id='ap_password']");
+    I.fillField("//input[@id='ap_password']","Mert.4141");
+    I.waitForElement("//input[@id='signInSubmit']");
+    I.click("//input[@id='signInSubmit']");
+    I.seeElement("//*[contains(test(),'Hello Mert')]");
+
+
+});
